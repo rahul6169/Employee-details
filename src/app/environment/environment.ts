1a83e5ad9d84cd4a72ba681e6756ae15f1607908
rahul6169 <rahulbalakrishnan6169@gmail.com>
2023-06-02 16:44:49 +0530

    ad

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  graphqlUri: "http://localhost:3333/graphql",
+  // graphqlUri: "http://localhost:3333/graphql",
+  graphqlUri: " https://562b-103-207-6-22.ngrok-free.app/graphql",
 };
